0aa2565bbee11de3ea752b4b28caeb68aec795b5
mickael borrajo <mick.borrajo.ael@gmail.com>
2021-02-23 20:55:48 +0100

    add validation to user and vehicle model. Model tested OK

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :vehicles, dependent: :destroy
+  validates :email, :password, :nick_name, presence: true
 end
