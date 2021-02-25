d5c28b8d335c9cc0a3d9d2989742379dd1cc0c9a
Shadya Jaquier <shadya.jaquier27@gmail.com>
2021-02-25 20:07:37 +0100

    Created new, create, vehicle_params in VehiclesController

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -3,4 +3,20 @@ class VehiclesController < ApplicationController
   def index
     @vehicles = Vehicle.all
   end
+
+  def new
+    @user = current_user
+    @vehicle = Vehicle.new
+  end
+
+  def create
+    @vehicle.create(vehicle_params)
+    redirect_to vehicles_path
+  end
+
+  private
+
+  def vehicle_params
+    params.require(:vehicle).permit(:description, :cost_per_day, :category, :availability, :name, :user_id)
+  end
 end
