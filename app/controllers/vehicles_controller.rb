db0b2a0230b5cd43e3b78a7ebb823588d3fff8c4
reschahmadi <ahm.res.ahmadi@gmail.comi>
2021-02-27 12:51:29 +0100

    Vehicle Detail Page

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -3,4 +3,28 @@ class VehiclesController < ApplicationController
   def index
     @vehicles = Vehicle.all
   end
+
+  def new
+    @vehicle = Vehicle.new
+  end
+
+  def create
+    @vehicle = Vehicle.new(vehicle_params)
+    @vehicle.user = current_user
+    if @vehicle.save!
+      redirect_to vehicles_path, notice: 'You successfully added a new vehicle.'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @vehicle = Vehicle.find_by(id: params[:id])
+  end
+
+  private
+
+  def vehicle_params
+    params.require(:vehicle).permit(:description, :cost_per_day, :category, :availability, :name, :user_id)
+  end
 end
