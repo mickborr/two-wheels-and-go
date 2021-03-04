e831d7ffb52d8aa024282c34d5e5c86e5a84ae20
mickael borrajo <mick.borrajo.ael@gmail.com>
2021-03-04 20:54:57 +0100

    add a city columne to vheicle model

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,9 +1,11 @@
 class Vehicle < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   belongs_to :user
   validates :category, inclusion: { in:
     ['motorbike', 'electric motorbike', 'bicycle', 'electric bicycle', 'scooter',
      'electric scooter', 'kick scooter', 'electric kick scooter'] }
   validates :description, length: { in: 20..350 }, presence: true
-  validates :name, :cost_per_day, presence: true
+  validates :name, :cost_per_day, :city, :address, presence: true
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_192508) do
+ActiveRecord::Schema.define(version: 2021_03_04_194801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_03_04_192508) do
     t.float "latitude"
     t.float "longitude"
     t.string "address"
+    t.string "city"
     t.index ["user_id"], name: "index_vehicles_on_user_id"
   end
 
